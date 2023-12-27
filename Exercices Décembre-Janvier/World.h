f51e513fd4c6d9d673e56432bc40eb5706c9447f
Eliott-Guignabaudet <eliottg@break-first.com>
2023-12-27 17:18:48 +0100

    [UPDATE] World: Change function unuse in main to private

--- Exercices Décembre-Janvier/World.h
+++ Exercices Décembre-Janvier/World.h
@@ -7,14 +7,16 @@ class World
 private:
 	std::vector<Entity*> m_entities;
 	void Init();
+	void TryRemoveDeadEntity(Alive* a_target);
+	template<class T = Entity>
+	T* GetClosest(Vector2 a_position);
+
+
 public:
 	World();
 	~World();
 	void Step();
-	void TryRemoveDeadEntity(Alive* a_target);
 	bool CheckSimulationIsFinished();
 
 
-	template<class T = Entity>
-	T* GetClosest(Vector2 a_position);
 };
